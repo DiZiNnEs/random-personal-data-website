d2907c65c319b3bbbcbf2b012b0677319818378d
dizinnes <dizinnes228@gmail.com>
2020-06-05 02:13:48 +0600

    rnd-site: add gender.py functional

--- app/main.py
+++ app/main.py
@@ -3,9 +3,11 @@ from app import app
 
 from .logic import first_name as fn
 from .logic import last_name as ln
+from .logic import gender as gn
 
 
 @app.route('/')
 def index():
     return render_template('index.html', title='Fake Data', first_names=fn.first_name_function(),
-                           last_name=ln.last_name_function())
+                           last_name=ln.last_name_function(),
+                           gender=gn.random_gender())
