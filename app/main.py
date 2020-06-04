b4ed34a0f12e5e9ac4e15378e39d499b16ec1ec7
dizinnes <dizinnes228@gmail.com>
2020-06-05 01:50:21 +0600

    rnd-site: add first_name and last_name fake data functional

--- app/main.py
+++ app/main.py
@@ -1,6 +1,11 @@
+from flask import render_template
 from app import app
+
+from .logic import first_name as fn
+from .logic import last_name as ln
 
 
 @app.route('/')
 def index():
-    return 'Hello, World!'
+    return render_template('index.html', title='Fake Data', first_names=fn.first_name_function(),
+                           last_name=ln.last_name_function())
